cb2b21de649068a20bd47f9714ff91c8ba2c3b26
ravneetkangg <ravneetkang2003@gmail.com>
2025-07-20 14:54:56 +0530

    env in backend

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,30 @@
+require('dotenv').config();
 const express = require('express');
-const app = express();
 const cors = require('cors');
+
+const app = express();
 
 const dbConnect = require("./config/db");
 const auth = require('./routes/auth');
 const list = require('./routes/list');
 
+// Middleware
 app.use(express.json());
 app.use(cors());
 dbConnect();
 
+// Routes
 app.use("/api/v1", auth);
 app.use("/api/v2", list);
 
-const PORT = 4700;
+// Environment port
+const PORT = process.env.PORT || 4700;
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
 });
 
+// Root route
 app.get('/', (req, res) => {
     res.send("Hello, World!");
 });
--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,14 @@
 const mongoose = require("mongoose");
+require('dotenv').config(); // Load .env variables
 
 const dbConnect = () => {
-    mongoose.connect("mongodb+srv://ravneetkang2003:Qub96c6xBOb9nkak@cluster0.tdraycq.mongodb.net/Todo")
+    mongoose.connect(process.env.MONGODB_URI)
         .then(() => console.log("DB connection is successful"))
         .catch((error) => {
             console.log("Issue in DB connection");
             console.error(error.message);
             process.exit(1);
         });
-}
+};
 
 module.exports = dbConnect;
