057f273e4a8e77dab62a593c0d5ca5f0c65abc4a
ravneetkangg <ravneetkang2003@gmail.com>
2025-07-25 15:52:48 +0530

    get live 2

--- frontend/src/components/about/About.jsx
+++ frontend/src/components/about/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
     <div className="about-container">
 
     <h1>ABOUT US</h1>
-    <p> Lorem ipsumm dolor sit amet consectetur adipisici Lorem ipsum dolor sit amet Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi soluta hic corrupti nihil iure perspiciatis quis neque ratione nemo, eaque rerum, laboriosam doloribus impedit optio, quam ex? Recusandae, debitis nihil.consectetu
+    <p> Lorem ipsum dolor sit amet consectetur adipisici Lorem ipsum dolor sit amet Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi soluta hic corrupti nihil iure perspiciatis quis neque ratione nemo, eaque rerum, laboriosam doloribus impedit optio, quam ex? Recusandae, debitis nihil.consectetu
       r adipisicing elit. Sapiente quidem accusamus nostrum quo quas unde accusantium at minus et! Quasi odit excepturi eaque numquam, nemo corporis
        cum corrupti laboriosam eos!ng elit. Earum<br/> tenetur possimus iusto cumque, 
       quaerat perspiciatis facilis eveniet esse temporibus excepturi Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus ut dolore numquam itaque cum ipsam odit quisquam earum reprehenderit, odio molestiae atque distinctio. Cupiditate a perspiciatis harum ex quod <consectetur className="lorem"></consectetur>cupiditate atque amet enim sit voluptates inventore iste provident non</p>
